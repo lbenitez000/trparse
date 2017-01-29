1f932d882a98b062b540b6f4bc2ecb0f35b92e97
Luis Benitez <lbenitez000@gmail.com>
2017-01-29 17:47:21 -0300

    Add basic test

---
+++ test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+import glob
+import trparse
+
+def main():
+    
+    if len(sys.argv) == 1:
+        print "Missing test files"
+        sys.exit(1)
+
+    for file_pattern in sys.argv[1:]:
+
+        filenames = glob.glob(file_pattern)
+
+        for filename in filenames:
+            with open(filename, 'r') as f:
+                
+                print "Loading {}".format(filename)
+                tr = trparse.load(f)
+
+                print "Dumping"
+                print tr
+
+if __name__ == "__main__":
+    main()
